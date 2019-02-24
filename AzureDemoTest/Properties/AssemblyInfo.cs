35f2371d0227a2e373431cdc28015a52164bcd0d
SeanAudley <sean.audley@btinternet.com>
2019-02-24 15:17:23 +0000

    Add files via upload
    initial commit

---
+++ AzureDemoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureDemoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureDemoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26582d07-ce30-49f9-b38b-cb999fc5d100")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureDemoTest/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Net;
+
+namespace AzureDemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Get_Number1andNumber_2NotDefined()
+        {
+            var client = MakeClient();
+            var request = MakeRequest();
+            var response = client.Get(request);
+            Assert.AreEqual("\"Number 1 not passed\"", response.Content);
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void Get_Number2_NotDefined()
+        {
+            var client = MakeClient();
+            var request = MakeRequest();
+            request.AddQueryParameter("Number1", "4");
+            var response = client.Get(request);
+            Assert.AreEqual("\"Number 2 not passed\"", response.Content);
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void Get_Number1_NotDefined()
+        {
+            var client = MakeClient();
+            var request = MakeRequest();
+            request.AddQueryParameter("Number2", "3");
+            var response = client.Get(request);
+            Assert.AreEqual("\"Number 1 not passed\"", response.Content);
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+
+        [TestMethod]
+        public void Get_Number1andNumber2_Defined()
+        {
+            var client = MakeClient();
+            var request = MakeRequest();
+            request.AddQueryParameter("Number1", "5");
+            request.AddQueryParameter("Number2", "6");
+            var response = client.Get(request);
+            Assert.AreEqual("11", response.Content);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        private IRestClient MakeClient()
+        {
+            var url = ConfigurationManager.AppSettings["Url"];
+            var client = new RestClient(url);
+
+            return client;
+        }
+
+        private IRestRequest MakeRequest()
+        {
+            var appKey = ConfigurationManager.AppSettings["AzureAppKey"];
+            var SimpleFunctionName = ConfigurationManager.AppSettings["functionName"];
+            var Request = new RestRequest(SimpleFunctionName, Method.GET);
+            Request.AddQueryParameter("Code", appKey);
+            return Request;
+
+        }
+    }
+}
